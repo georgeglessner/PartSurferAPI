57c8727aa73d064a6a26b25f12415e55c333661b
George Glessner <glessneg@mail.gvsu.edu>
2019-02-25 16:26:28 -0700

    Formatting

--- app.py
+++ app.py
@@ -1,37 +1,45 @@
+import requests
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 from collections import defaultdict
-import requests
 from flask import Flask, request
 from flask_restful import Resource, Api, abort, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class Parts(Resource):
 
+class Parts(Resource):
     def get(self, name):
         # Grab the page
-        response = requests.get("https://partsurfer.hpe.com/Search.aspx?searchText={}".format(name))
+        response = requests.get(
+            "https://partsurfer.hpe.com/Search.aspx?searchText={}".format(name)
+        )
 
         # Find component BOMS
-        selection = Selector(response=response).xpath("//table//tr[contains(@class, 'RowStyle')]//span[contains(@id, 'ctl00_BodyContentPlaceHolder_gridCOMBOM')]//text()").getall()
+        selection = (
+            Selector(response=response)
+            .xpath(
+                "//table//tr[contains(@class, 'RowStyle')]//span[contains(@id, 'ctl00_BodyContentPlaceHolder_gridCOMBOM')]//text()"
+            )
+            .getall()
+        )
 
         # Initialize dictionary and variables
         results = defaultdict(list)
-        i = 0;
-        hpid = 0;
+        index = 0
+        hpid = 0
 
         # Only grab part and description
         for entry in selection:
-            if i == 0:
+            if index == 0:
                 results[hpid].append({"Part": entry})
-            if i == 1:
+            if index == 1:
                 results[hpid].append({"Description": entry})
-            if i == 2:
+            if index == 2:
                 hpid += 1
-                i = -1 
-            i+=1
+                index = -1
+            index += 1
 
         return {"results": results}
 

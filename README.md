66ac871a5c432dc83cbed73728e186576191dfbe
George Glessner <glessneg@mail.gvsu.edu>
2019-02-25 15:15:18 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# HPE Partsurfer API
+
+## ENDPOINT
+`http://68.2.92.148:5000/hpe/part/{partNumber}`
---
+++ app.py
@@ -0,0 +1,42 @@
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+from flask import Flask, request
+from flask_restful import Resource, Api, abort, reqparse
+
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+from collections import defaultdict
+import requests
+
+app = Flask(__name__)
+api = Api(app)
+
+class Parts(Resource):
+
+    def get(self, name):
+        response = requests.get("https://partsurfer.hpe.com/Search.aspx?searchText={}".format(name))
+        selection = Selector(response=response).xpath("//table//tr[contains(@class, 'RowStyle')]//span[contains(@id, 'ctl00_BodyContentPlaceHolder_gridCOMBOM')]//text()").getall()
+
+        # results = {}
+        results = defaultdict(list)
+        i = 0;
+        hpid = 0;
+
+        for entry in selection:
+            if i == 0:
+                results[hpid].append({"Part": entry})
+            if i == 1:
+                results[hpid].append({"Description": entry})
+            if i == 2:
+                hpid += 1
+                i = -1 
+            i+=1
+
+        return {"results": results}
+
+
+# File paths and params
+api.add_resource(Parts, "/hpe/part/<string:name>")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
